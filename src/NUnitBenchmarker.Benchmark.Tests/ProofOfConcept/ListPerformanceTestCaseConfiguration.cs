90035b2547070edf3de1e0a857eab77d0d951079
QualityHU <horo@controll.hu>
2014-02-25 17:35:00 +0100

    (Re)Adding accidentally untracked file File name ListPerformanceTestCaseConfiguration accidentally untracked by VS git

---
+++ src/NUnitBenchmarker.Benchmark.Tests/ProofOfConcept/ListPerformanceTestCaseConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnitBenchmarker.Benchmark.Configuration;
+
+namespace NUnitBenchmarker.Benchmark.Tests.ProofOfConcept
+{
+	/// <summary>
+	/// Class ListPerformanceTestCaseConfiguration.
+	/// TODO: Revise if generic T parameter is useful or overengineered
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class ListPerformanceTestCaseConfiguration<T> : PerformanceTestCaseConfigurationBase
+	{
+		private readonly Random random;
+		
+		public int Size { get; set; }
+		public int DummyForTesting { get; set; }
+
+		/// <summary>
+		/// Returns a <see cref="System.String" /> that represents this instance.
+		/// </summary>
+		/// <returns>A <see cref="System.String" /> that represents this instance.</returns>
+		public override string ToString()
+		{
+			return string.Format("{0}", Size);
+		}
+	}
+}
