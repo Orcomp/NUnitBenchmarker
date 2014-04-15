5eba83024b019937c70cda1c0efce2620596b89b
Geert van Horrik <geert@catenalogic.com>
2014-04-15 18:11:42 +0200

    Improved user experience with searching in type tree

---
+++ src/NUnitBenchmarker.UI/Converters/CollectionToVisibilityConverter.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CollectionToVisibilityConverter.cs" company="Orcomp development team">
+//   Copyright (c) 2008 - 2014 Orcomp development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace NUnitBenchmarker.Converters
+{
+    using System;
+    using System.Collections;
+    using System.Windows;
+    using Catel.MVVM.Converters;
+
+    public class CollectionToVisibilityConverter : VisibilityConverterBase
+    {
+        #region Constructors
+        public CollectionToVisibilityConverter()
+            : base(Visibility.Hidden)
+        {
+        }
+        #endregion
+
+        protected override bool IsVisible(object value, Type targetType, object parameter)
+        {
+            bool isVisible = false;
+
+            var collection = value as ICollection;
+            if (collection != null)
+            {
+                isVisible = collection.Count > 0;
+            }
+
+            var invertParameter = parameter as string;
+            if (!string.IsNullOrWhiteSpace(invertParameter))
+            {
+                bool invert = false;
+                bool.TryParse(invertParameter, out invert);
+                if (invert)
+                {
+                    isVisible = !isVisible;
+                }
+            }
+
+            return isVisible;
+        }
+    }
+}
